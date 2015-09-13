b4709ee593195313f8a3a315debb6bdd19b6058e
Niall Wallace <nj.wallace@outlook.com>
2015-09-13 22:00:05 +0100

    added finctionality to add an attack using other table params

--- cow_app/app/controllers/attack_controller.rb
+++ cow_app/app/controllers/attack_controller.rb
@@ -11,5 +11,25 @@ class AttackController < ApplicationController
     @localAttacks = Attack.where(location: @attack.location)
   end
 
+   def new
+    @attack = Attack.new
+    render :new
+  end
+
+  def create
+    attack = Attack.new(attack_params)
+    
+    if attack.save
+      redirect_to '/attack'
+    else
+      render :new
+    end
+  end
+
+    private
+  def attack_params
+    params.require(:attack).permit(:location, :rating, :person_id, :cow_id)
+  end
+
 
 end
