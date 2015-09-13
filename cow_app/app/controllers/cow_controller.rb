04bdb3e5e927bc7cc5685f9cad140fff6b1d0eb3
Niall Wallace <nj.wallace@outlook.com>
2015-09-13 14:09:41 +0100

    added show functionality for cow

--- cow_app/app/controllers/cow_controller.rb
+++ cow_app/app/controllers/cow_controller.rb
@@ -1,6 +1,20 @@
 class CowController < ApplicationController
-end
 
-def cow 
+
+  def index
+    @title = 'Home'
+    @cows = Cow.all
+
+  end
+
+
+  def show
+    @title = 'Have you seen this Cow?'
+    @cow = Cow.find(params[:id])
+
+    @attacks = Attack.find(@cow.id)
+  end
+
+
 
 end
--- cow_app/db/seeds.rb
+++ cow_app/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Cow.create(name: 'Kevin', age: 33)
+Cow.create(name: 'Bob', age: 13)
+
+Person.create(name: 'Linda', age: 41)
+Person.create(name: 'Niall', age: 23)
+Person.create(name: 'James', age: 66)
+Person.create(name: 'Ambar', age: 25)
+
+
+# location, rating, person id, cow id
+
+Attack.create(location: 'London', rating: 5, person_id: 1, cow_id: 2)
+Attack.create(location: 'Newcastle', rating: 5, person_id: 2, cow_id: 1)
+Attack.create(location: 'Manchester', rating: 2, person_id: 1, cow_id: 2)
+Attack.create(location: 'Blackpool', rating: 5, person_id: 3, cow_id: 2)
+Attack.create(location: 'London', rating: 4, person_id: 4, cow_id: 1)
+Attack.create(location: 'Leeds', rating: 1, person_id: 1, cow_id: 2)
+Attack.create(location: 'London', rating: 2, person_id: 1, cow_id: 1)
+
+puts 'Seed Complete!'
+puts "Cows: #{Cow.all.count}"
+puts "People: #{Person.all.count}"
+puts "Attacks: #{Attack.all.count}"
