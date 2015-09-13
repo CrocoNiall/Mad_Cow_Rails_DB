3f112433770efb5d9dbedd86a176f0fb6c27d837
Niall Wallace <nj.wallace@outlook.com>
2015-09-13 22:12:47 +0100

    Added functionality to edit attack

--- cow_app/app/controllers/attack_controller.rb
+++ cow_app/app/controllers/attack_controller.rb
@@ -26,6 +26,26 @@ class AttackController < ApplicationController
     end
   end
 
+  def edit
+    @attack = Attack.find(params[:id])
+  end
+
+  def update
+    attack = Attack.find(params[:id])
+
+    if attack.update(attack_params)
+      redirect_to attack
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    cow = Attack.find(params[:id]).destroy
+
+    redirect_to '/attack'
+  end
+
     private
   def attack_params
     params.require(:attack).permit(:location, :rating, :person_id, :cow_id)
